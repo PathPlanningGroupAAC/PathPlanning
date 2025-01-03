27050cc025e480daaf87ed50e43a8c3a3c686d98
PathPlanningGroupAAC <pathplanneraac@gmail.com>
2025-01-03 01:47:39 +0100

    Update DebugUtil.cpp

--- DebugUtil.cpp
+++ DebugUtil.cpp
@@ -61,7 +61,8 @@ void debug_plot(const std::vector<glm::vec2>& cones_blue, const std::vector<glm:
 // giallo out
 void loadTrack(std::vector<glm::vec2>& points, const std::string& path)
 {
-    std::string absolute = std::filesystem::current_path().string() + std::filesystem::path::preferred_separator + path;
+    char separator = std::filesystem::path::preferred_separator;
+    std::string absolute = std::filesystem::current_path().string() + separator + path;
     try {
         io::CSVReader<2> in(absolute);
 
